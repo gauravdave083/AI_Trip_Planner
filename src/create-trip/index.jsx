eb6616e44660b1304a0a1c8d139f8857a66bfe49
gauravdave083 <gd836398@gmail.com>
2024-11-07 13:51:37 +0530

    phase 2

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,11 +1,36 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { SelectBudgetOptions, SelectTravelesList } from '@/constants/options';
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 
 function CreateTrip() {
      const [place, setPlace] = useState();
+
+     const [formData, setFormData]=useState([]);
+
+     const handleInputChange=(name, value)=>{
+
+
+          setFormData({
+               ...formData,
+               [name]:value
+          })
+     }
+
+     useEffect(()=>{
+          console.log(formData);
+     }, [formData])
+
+     const OnGenerateTrip=()=>{
+          if(formData?.noOfDays>30)
+               {
+                    console.log("Please enter Trip Days less than 30")
+                    return ;
+               }
+          
+          console.log(formData);
+     }
 
      return (
      <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
@@ -19,21 +44,27 @@ function CreateTrip() {
                     apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
                     selectProps={{
                          place, 
-                         onChange:(v)=> {setPlace(v);console.log(v)}
+                         onChange:(v)=> {setPlace(v); handleInputChange('location',v )}
                     }}
                />
                </div>
 
                <div>
                <h2 className='text-xl my-3 font-medium'>How many days are you planning your trip ?</h2>
-               <Input placeholder={'Ex. 3'} type="number"/>
+               <Input placeholder={'Ex. 3'} type="number"
+               onChange={(e)=> handleInputChange('noOfDays', e.target.value)}
+               />
                </div>
 
                <div>
                     <h2 className='text-xl my-3 font-medium'>What is your budget ?</h2>
                     <div className='grid grid-cols-3 gap-5 mt-5'>
                          {SelectBudgetOptions.map((item,index)=>(
-                              <div key={index} className='p-4 border cursor-pointer rounded-lg hover:shadow-lg'>
+                              <div key={index} 
+                              onClick={()=> handleInputChange('budget', item.title)}
+                              className={`p-4 border cursor-pointer rounded-lg hover:shadow-lg
+                              ${formData?.budget==item.title&&'shadow-lg border-black'}
+                              `}>
                                    <h2 className='text-4xl'>{item.icon}</h2>
                                    <h2 className='font-bold text-lg'>{item.title}</h2>
                                    <h2 className='text-sm text-gray-500'>{item.desc}</h2>
@@ -45,7 +76,11 @@ function CreateTrip() {
                     <h2 className='text-xl my-3 font-medium'>Who do you plan on travelling with on your next adventure ?</h2>
                     <div className='grid grid-cols-3 gap-5 mt-5'>
                          {SelectTravelesList.map((item,index)=>(
-                              <div key={index} className='p-4 border cursor-pointer rounded-lg hover:shadow-lg'>
+                              <div key={index} 
+                              onClick={()=> handleInputChange('traveler', item.people)}
+                              className={`p-4 border cursor-pointer rounded-lg hover:shadow-lg
+                                   ${formData?.traveler==item.people&&'shadow-lg border-black'}
+                                   `}>
                                    <h2 className='text-4xl'>{item.icon}</h2>
                                    <h2 className='font-bold text-lg'>{item.title}</h2>
                                    <h2 className='text-sm text-gray-500'>{item.desc}</h2>
@@ -57,7 +92,7 @@ function CreateTrip() {
 
 
           <div className='my-10 justify-end flex'>
-               <Button>Generate Trip</Button>
+               <Button onClick={OnGenerateTrip}>Generate Trip</Button>
           </div>
 
      </div>
