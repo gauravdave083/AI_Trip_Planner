7c84cf54ef62c3785c3b5d84afec25056f8fef32
gauravdave083 <gd836398@gmail.com>
2024-11-07 08:25:02 +0530

    phase 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import { Button } from './components/ui/button'
+import Hero from './components/custom/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <Button>Subscribe</Button>
-      </div>
+      {/* Hero */}
+      <Hero/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import CreateTrip from './create-trip';
+import Header from './components/custom/Header'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: '/create-trip',
+    element: <CreateTrip/>
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header/>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
